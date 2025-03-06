f400e1223685a2cfc7bd9dfab7cec229caf128dc
Bryce-3D <bryceainsleysanchez@gmail.com>
2025-03-06 22:39:24 +0800

    Add `Remark` to the model
    https://se-education.org/guides/tutorials/ab3AddRemark.html#add-remark-to-the-model

---
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -0,0 +1,37 @@
+// Copied from https://github.com/se-edu/addressbook-level3/commit/
+// 6da0fc1cf53f8b2758e04c56c5217e6f7fb8d118#diff-41bb13c581e280c686198251ad6cc3
+// 37cd5e27032772f06ed9bf7f1440995ece
+
+package seedu.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Represents a Person's remark in the address book.
+ * Guarantees: immutable; is always valid
+ */
+public class Remark {
+    public final String value;
+
+    public Remark(String remark) {
+        requireNonNull(remark);
+        value = remark;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Remark // instanceof handles nulls
+                && value.equals(((Remark) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+}
